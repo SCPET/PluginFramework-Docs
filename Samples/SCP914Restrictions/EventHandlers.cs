9e1873519d807ca756eb87f65a99937b793e644b
Thundermaker300 <thundermaker300@gmail.com>
2021-08-10 14:49:37 -0400

    Adding comment documentation to SCP914Restrictions

--- Samples/SCP914Restrictions/EventHandlers.cs
+++ Samples/SCP914Restrictions/EventHandlers.cs
@@ -5,30 +5,32 @@ namespace ETTestingPlugin
     public class EventHandlers
     {
         public MainPlugin plugin;
-        public int uses = -1;
+        public int uses = -1; // This variable is used by the rest of the code to determine how many uses are left. It is reset on round start.
 
         public EventHandlers(MainPlugin plugin)
         {
             this.plugin = plugin;
         }
 
-        public void OnRoundStart(RoundStartEvent ev)
+        public void OnRoundStart(RoundStartEvent ev) // Round Start event
         {
-            uses = plugin.Config.UsesPerRound;
+            uses = plugin.Config.UsesPerRound; // Sets the uses. This is set on round start so that uses are reset at the start of every round.
         }
 
-        public void OnScp914Activate(Scp914ActivateEvent ev)
+        public void OnScp914Activate(Scp914ActivateEvent ev) // SCP-914 Activate event
         {
+            // Finalized = The event has already happened, and it cannot be Disallowed().
+            // !Finalized = The event has not occurred yet, and it can be disallowed by any plugin.
 
-            if (uses <= 0 && !ev.Finalized)
+            if (uses <= 0 && !ev.Finalized) // Checks if there are no uses and SCP-914 hasn't been activated yet. Disallows if both of these are true. You CANNOT call Disallow() if the event is Finalized.
             {
-                ev.Player.Broadcast(plugin.Config.FailMessage, 5f);
-                ev.Disallow();
+                ev.Player.Broadcast(plugin.Config.FailMessage, 5f); // Displays the "FailMessage" config to the player who attempted to activate SCP-914.
+                ev.Disallow(); // Prevents the event from executing.
             }
-            else if (uses > 0 && ev.Finalized)
+            else if (uses > 0 && ev.Finalized) // Checks if there are uses left and if the event IS Finalized. Other plugins can disallow this event, so it is good practice to ensure that the event DID actually happen before taking away a usage.
             {
-                uses--;
-                ev.Player.Broadcast(plugin.Config.UsesLeftMessage.Replace("%COUNT%", uses.ToString()), 5f);
+                uses--; // Removes one usage.
+                ev.Player.Broadcast(plugin.Config.UsesLeftMessage.Replace("%COUNT%", uses.ToString()), 5f); // Displays the "UsesLeftMessage" config to the player, replacing "%COUNT%" with the amount of uses remaining.
             }
         }
     }
--- Samples/SCP914Restrictions/MainPlugin.cs
+++ Samples/SCP914Restrictions/MainPlugin.cs
@@ -7,25 +7,25 @@ namespace ETTestingPlugin
 {
     public class MainPlugin : Plugin<PluginConfig>
     {
-        public override string Name => "SCP914Restrictions";
-        public override string Author => "Thunder";
-        public override Version Version => new Version(1, 0, 0);
-        public override PluginPriority Priority => PluginPriority.Low;
+        public override string Name => "SCP914Restrictions"; // You must set the name of the plugin.
+        public override string Author => "Thunder"; // The author of the plugin.
+        public override Version Version => new Version(1, 0, 0); // The version of the plugin. This MUST be changed when updates are released.
+        public override PluginPriority Priority => PluginPriority.Low; // Tells the server that this plugin has a lower-than-standard loading priority.
 
         EventHandlers handlers;
 
         public override void OnEnabled()
         {
-            handlers = new EventHandlers(this);
-            ServerEvents.RoundStart += handlers.OnRoundStart;
-            ScpEvents.Scp914Activate += handlers.OnScp914Activate;
+            handlers = new EventHandlers(this); // Creates an EventHandlers class to handle events.
+            ServerEvents.RoundStart += handlers.OnRoundStart; // Subscribes to the round start event.
+            ScpEvents.Scp914Activate += handlers.OnScp914Activate; // Subscribes to the SCP-914 activate event.
         }
 
         public override void OnDisabled()
         {
-            ServerEvents.RoundStart -= handlers.OnRoundStart;
-            ScpEvents.Scp914Activate -= handlers.OnScp914Activate;
-            handlers = null;
+            ServerEvents.RoundStart -= handlers.OnRoundStart; // Unsubscribes from the round start event.
+            ScpEvents.Scp914Activate -= handlers.OnScp914Activate; // Unsubscribes from the SCP-914 activate event.
+            handlers = null; // Remove the EventHandlers class, since we no longer need it.
         }
     }
 }
